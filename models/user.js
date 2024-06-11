f2e961efc875556c37ed7bbd8eb604604bdce15d
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-06-11 11:21:20 -0500

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      emun: ["ADMIN_ROLE", "USER_ROLE"],
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...user } = this.toObject();
+  user.uid = _id;
+  return user;
+};
+
+module.exports = model("User", UserSchema);
