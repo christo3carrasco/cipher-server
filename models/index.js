2f9481158a196ae90a845bbab7a5f8b779645a4f
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-06-11 11:21:32 -0500

    feat: add role model

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
+const Role = require("./role");
+const User = require("./user");
 const Vote = require("./vote");
 const Voter = require("./voter");
 const Voting = require("./voting");
 
 module.exports = {
+  Role,
+  User,
   Vote,
   Voter,
   Voting,
